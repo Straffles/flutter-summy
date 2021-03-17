9c2cf144ffe25b3f3de32843e9f4cd78e2a30472
Straffles <51118216+Straffles@users.noreply.github.com>
2021-03-17 14:00:59 +0700

    Add files via upload

---
+++ API.py
@@ -0,0 +1,21 @@
+from flask import Flask ,request, jsonify
+import json
+from vitest import summary
+
+response = ''
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['POST'])
+def summarize():
+    global response
+
+    request_data = request.json
+    text = request_data['text']
+    text = summary(text)
+    return  jsonify({'text' : text})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888, debug=True)
