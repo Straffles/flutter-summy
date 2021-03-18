dad9e20e8ea0516b7d308dc98f4868489f2a589f
Straffles <51118216+Straffles@users.noreply.github.com>
2021-03-18 20:04:01 +0700

    Update

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask ,request, jsonify
+import json
+from vitest import summary
+
+response = ''
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def summarize():
+    global response
+
+    request_data = request.json
+    text = request_data['text']
+    text = summary(text)
+    return  jsonify({'text' : text})
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 5000, debug=True)
